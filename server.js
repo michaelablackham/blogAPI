1ea0e70c1015310e0fc4c4652126be5ee89c5aca
Michaela Blackham <Michaelablackham@gmail.com>
2017-08-14 17:44:14 -0600

    added all request

--- server.js
+++ server.js
@@ -29,10 +29,70 @@ BlogPosts.create(
   'Charlie day'
 );
 
+//simple get request to see json objects of blog posts
 app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get());
 });
 
+//post request to add new items to feed of blog posts
+app.post('/blog-posts', jsonParser, (req, res) => {
+  //require certain fields (found in modules.js)
+  const requiredFields = ['title','content','author'];
+  //loop through each required field
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    //if current require field item is not within the request body
+    if(!(field in req.body)) {
+      // console/return an error
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  //If all goog, create a new blog post from the request title, content and author
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  //show the rsponse within postman of the new json item if successful
+  res.status(201).json(item);
+});
+
+//DELETE request
+app.delete('/blog-posts/:id', (req, res) => {
+  //Get the parameter (id) and find blog post with that ID and delete
+  BlogPosts.delete(req.params.id);
+  res.status(204).end();
+});
+
+//Put request
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  //require certain fields (found in modules.js)
+  const requiredFields = ['title','content','author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    //if current require field item is not within the request body
+    if(!(field in req.body)) {
+      // console/return an error
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+  console.log(`Updating blog post \`${req.params.id}\``);
+  res.status(204).end();
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
